f0d5d687996730d130c748fe296215c8f4ef4131
vpk21 <Vincenttems@outlook.com>
2020-05-18 01:43:57 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+firstName = input("Enter first name: ")
+lastName = input("Enter your last name ")
+birth = int(input("Enter your birth "))
+streetAddress = input("Enter your address: ")
+zipCode = int(input("Enter your zipcode: "))
+a = 2020-birth
+print(f'The first name is {firstName} and his last name is {lastName}. The age is {a} the address is   {streetAddress} and the zipCode is {zipCode}')
